baef9b7bf2040cb66936580311ac328fe5832ff8
Michal Špaček <skim@cpan.org>
2005-08-28 19:06:30 +0000

    New version.

--- Perl.pm
+++ Perl.pm
@@ -1,7 +1,7 @@
 #------------------------------------------------------------------------------
 package Tag::Reader::Perl;
 #------------------------------------------------------------------------------
-# $Id: Perl.pm,v 1.13 2005-08-27 17:52:18 skim Exp $
+# $Id: Perl.pm,v 1.14 2005-08-28 19:06:30 skim Exp $
 
 # Pragmas.
 use strict;
@@ -44,16 +44,15 @@ sub set_text {
 	if (! $text) {
 		err "Bad text.";
 	}
-	if (! $force && defined $self->{'text'}) {
-		err "Cannot set text if exists text.";
+	if (! $force && (defined $self->{'text'} 
+		|| defined $self->{'filename'})) {
+
+		err "Cannot set new data if exists data.";
 	}
 	$self->{'text'} = $text;
 
-	# Default values.
-	$self->{'charpos'} = 0;
-	$self->{'tagcharpos'} = 0;
-	$self->{'textline'} = 1;
-	$self->{'tagline'} = 0;
+	# Reset values.
+	$self->_reset;
 }
 
 #------------------------------------------------------------------------------
@@ -65,21 +64,18 @@ sub set_file {
 	if (! $file || ! -r $file) {
 		err "Bad file.";
 	}
-	if (! $force && defined $self->{'text'}) {
-		err "Cannot set text if exists text.";
+	if (! $force && (defined $self->{'text'} 
+		|| defined $self->{'filename'})) {
+
+		err "Cannot set new data if exists data.";
 	}
 	if (! open(INF, "<$file")) {
-		err "Cannot read file \"$file\".";
+		err "Cannot open file '$file'.";
 	}
-	foreach (<INF>) {
-		$self->{'text'} .= $_;
-	}
+	$self->{'filename'} = *INF;
 
-	# Default values.
-	$self->{'charpos'} = 0;
-	$self->{'tagcharpos'} = 0;
-	$self->{'textline'} = 1;
-	$self->{'tagline'} = 0;
+	# Reset values.
+	$self->_reset;
 }
 
 #------------------------------------------------------------------------------
@@ -90,10 +86,9 @@ sub gettoken {
 	my $self = shift;
 
 	# Stay.
-	my $stay = 0;
-	my $spec_stay = 0;
-	my $old_stay = 0;
-	my $comment_stay = 0;
+	$self->{'stay'} = 0;
+	$self->{'spec_stay'} = 0;
+	$self->{'old_stay'} = 0;
 
 	# Data.
 	$self->{'data'} = [];
@@ -103,203 +98,34 @@ sub gettoken {
 	$self->{'tag_length'} = 0;
 
 	# Braces.
-	my ($brace, $bracket) = (0, 0);
+	($self->{'brace'}, $self->{'bracket'}) = (0, 0);
 
 	# Quote.
-	my $quote = '';
+	$self->{'quote'} = '';
 
 	# Tag line.
 	$self->{'tagline'} = $self->{'textline'};
 	$self->{'tagcharpos'} = 0;
 
-	# Foreach chars.
-	while (defined $self->{'text'} 
-		&& defined (my $char = substr($self->{'text'}, 0, 1))) { 
+	if (exists $self->{'text'}) {
+		while (exists $self->{'text'} 
+			&& $self->{'stay'} < 98
+			&& defined ($self->{'char'} 
+			= substr($self->{'text'}, 0, 1))) { 
 
-		# Char position.
-		$self->{'charpos'}++;
-
-		# Normal tag.
-		if ($spec_stay == 0) {
-
-			# Begin of normal tag.
-			if ($stay == 0 && $char eq '<') {
-
-				# In tag.
-				if ($#{$self->{'data'}} == -1) {
-					$self->{'tagcharpos'} 	
-						= $self->{'charpos'};
-					$stay = 1;
-					push @{$self->{'data'}}, $char;
-					$self->{'tag_length'} = 1;
-
-				# Start of tag, after data.
-				} else {
-					$stay = 99;
-				}
-
-			# Text.
-			} elsif ($stay == 0) {
-				push @{$self->{'data'}}, $char;
-				if ($self->{'tagcharpos'} == 0) {
-					$self->{'tagcharpos'} 
-						= $self->{'charpos'};
-				}
-
-			# In a normal tag.
-			} elsif ($stay == 1) {
-
-				# End of normal tag.
-				if ($char eq '>') {
-					$stay = 98;
-					$self->_tag_type;
-					push @{$self->{'data'}}, $char;
-					$self->{'tag_length'} = 0;
-
-				# First charcter after '<' in normal tag.
-				} elsif ($self->{'tag_length'} == 1 
-					&& _is_first_char_of_tag($char)) {
-
-					if ($char eq '!') {
-						$spec_stay = 1;
-					}
-					push @{$self->{'data'}}, $char;
-					$self->{'tag_length'}++;
-
-				# Next character in normal tag (name).
-				} elsif ($self->{'tag_length'} > 1
-					&& _is_in_tag_name($char)) {
-
-					push @{$self->{'data'}}, $char;
-					$self->{'tag_length'}++;
-
-				# Other characters.
-				} else {
-					if ($self->{'tag_length'} == 1 
-						|| $char eq '<') {
-
-						err "Bad tag.";
-					}
-					$self->_tag_type;
-					push @{$self->{'data'}}, $char;
-				}
-			}
-
-		# Other tags.
-		} else {
-
-			# End of normal tag.
-			if ($char eq '>') { 
-				if (($brace == 0 && $bracket == 0
-					&& $spec_stay < 3) 
-
-					# Comment.
-					|| ($spec_stay == 3 
-					&& join('', 
-					@{$self->{'data'}}[-2 .. -1]) 
-					eq '--')
-
-					# CDATA.
-					|| ($self->{'tag_type'} =~ /^!\[cdata\[/
-					&& join('', 
-					@{$self->{'data'}}[-2 .. -1]) 
-					eq ']]')) {
-
-					$stay = 98;
-					$spec_stay = 0;
-					$self->{'tag_length'} = 0;
-				}
-				if ($spec_stay != 4) {
-					$bracket--;
-				}
-				push @{$self->{'data'}}, $char;
-
-			# Comment.
-			} elsif ($spec_stay == 3) {
-
-				# '--' is bad.
-				if ($self->{'tag_length'} == 0
-					&& join('', @{$self->{'data'}}
-					[-2 .. -1]) eq '--') {
-
-					err "Bad tag.";
-				}
-				$self->_tag_type;
-				push @{$self->{'data'}}, $char;
-
-			# Quote.
-			} elsif ($spec_stay == 4) {
-				if ($char eq $quote) {
-					$spec_stay = $old_stay;
-					$quote = '';
-				}
-				push @{$self->{'data'}}, $char;
-
-			} elsif ($char eq ']') {
-				push @{$self->{'data'}}, $char;
-				$brace--;
-
-			# Next character in normal tag (name).
-			} elsif ($self->{'tag_length'} > 1
-				&& _is_in_tag_name($char)) {
-
-				# Comment detect.
-				if (($self->{'tag_length'} == 2
-					|| $self->{'tag_length'} == 3)
-					&& $char eq '-') {
-				
-					$spec_stay++;
-				}
-				if ($char eq '[') {
-					$brace++;
-				}
-				push @{$self->{'data'}}, $char;
-				$self->{'tag_length'}++;
-
-			# Other characters.
-			} else {
-				if ($quote eq '' && $char eq '"') {
-					$quote = '"';
-					$old_stay = $spec_stay;
-					$spec_stay = 4;
-				}
-				if ($quote eq '' && $char eq "'") {
-					$quote = "'";
-					$old_stay = $spec_stay;
-					$spec_stay = 4;
-				}
-				if ($char eq '<') {
-					$bracket++;
-				}
-				if ($char eq '[') {
-					$brace++;
-				}
-				$self->_tag_type;
-				push @{$self->{'data'}}, $char;
-			}
+			$self->_gettoken;
 		}
+	} elsif (exists $self->{'filename'}) {
+		while ($self->{'stay'} < 98 
+			&& ((defined ($self->{'char'} 
+			= shift @{$self->{'old_data'}})) 
+			|| (defined ($self->{'char'} 
+			= getc($self->{'filename'}))))) {
 
-		# Remove char from buffer.
-		if ($stay != 99) {
-			if (length $self->{'text'} > 1) {
-				$self->{'text'} = substr($self->{'text'}, 1);
-			} else {
-				$self->{'text'} = undef;
-			}
-		}
-		if ($stay == 98 || $stay == 99) {
-			if ($stay == 99) {
-				$self->{'charpos'}--;
-			}
-			last;
-		}
-
-		# Next line.
-		if ($char eq "\n") {
-			$self->{'textline'}++;
-			$self->{'charpos'} = 0;
+			$self->_gettoken;
 		}
 	}
+
 	my $data = join('', @{$self->{'data'}});
 	return () if $data eq '';
 	return wantarray ? ($data, $self->{'tag_type'}, $self->{'tagline'}, 
@@ -307,8 +133,227 @@ sub gettoken {
 }
 
 #------------------------------------------------------------------------------
-# Private functions.
+# Private functions and methods.
 #------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+sub _reset {
+#------------------------------------------------------------------------------
+# Reset class values.
+
+	my $self = shift;
+
+	# Default values.
+	$self->{'charpos'} = 0;
+	$self->{'tagcharpos'} = 0;
+	$self->{'textline'} = 1;
+	$self->{'tagline'} = 0;
+	$self->{'old_data'} = [];
+}
+
+#------------------------------------------------------------------------------
+sub _gettoken {
+#------------------------------------------------------------------------------
+# Main get token.
+
+	my $self = shift;
+
+	# Char position.
+	$self->{'charpos'}++;
+
+	# Normal tag.
+	if ($self->{'spec_stay'} == 0) {
+
+		# Begin of normal tag.
+		if ($self->{'stay'} == 0 && $self->{'char'} eq '<') {
+
+			# In tag.
+			if ($#{$self->{'data'}} == -1) {
+				$self->{'tagcharpos'} 	
+					= $self->{'charpos'};
+				$self->{'stay'} = 1;
+				push @{$self->{'data'}}, $self->{'char'};
+				$self->{'tag_length'} = 1;
+
+			# Start of tag, after data.
+			} else {
+				$self->{'stay'} = 99;
+			}
+
+		# Text.
+		} elsif ($self->{'stay'} == 0) {
+			push @{$self->{'data'}}, $self->{'char'};
+			if ($self->{'tagcharpos'} == 0) {
+				$self->{'tagcharpos'} 
+					= $self->{'charpos'};
+			}
+
+		# In a normal tag.
+		} elsif ($self->{'stay'} == 1) {
+
+			# End of normal tag.
+			if ($self->{'char'} eq '>') {
+				$self->{'stay'} = 98;
+				$self->_tag_type;
+				push @{$self->{'data'}}, $self->{'char'};
+				$self->{'tag_length'} = 0;
+
+			# First charcter after '<' in normal tag.
+			} elsif ($self->{'tag_length'} == 1 
+				&& _is_first_char_of_tag($self->{'char'})) {
+
+				if ($self->{'char'} eq '!') {
+					$self->{'spec_stay'} = 1;
+				}
+				push @{$self->{'data'}}, $self->{'char'};
+				$self->{'tag_length'}++;
+
+			# Next character in normal tag (name).
+			} elsif ($self->{'tag_length'} > 1
+				&& _is_in_tag_name($self->{'char'})) {
+
+				push @{$self->{'data'}}, $self->{'char'};
+				$self->{'tag_length'}++;
+
+			# Other characters.
+			} else {
+				if ($self->{'tag_length'} == 1 
+					|| $self->{'char'} eq '<') {
+
+					err "Bad tag.";
+				}
+				$self->_tag_type;
+				push @{$self->{'data'}}, $self->{'char'};
+			}
+		}
+
+	# Other tags.
+	} else {
+
+		# End of normal tag.
+		if ($self->{'char'} eq '>') { 
+			if (($self->{'brace'} == 0 
+				&& $self->{'bracket'} == 0
+				&& $self->{'spec_stay'} < 3) 
+
+				# Comment.
+				|| ($self->{'spec_stay'} == 3 
+				&& join('', 
+				@{$self->{'data'}}[-2 .. -1]) 
+				eq '--')
+
+				# CDATA.
+				|| ($self->{'tag_type'} =~ /^!\[cdata\[/
+				&& join('', 
+				@{$self->{'data'}}[-2 .. -1]) 
+				eq ']]')) {
+
+				$self->{'stay'} = 98;
+				$self->{'spec_stay'} = 0;
+				$self->{'tag_length'} = 0;
+			}
+			if ($self->{'spec_stay'} != 4) {
+				$self->{'bracket'}--;
+			}
+			push @{$self->{'data'}}, $self->{'char'};
+
+		# Comment.
+		} elsif ($self->{'spec_stay'} == 3) {
+
+			# '--' is bad.
+			if ($self->{'tag_length'} == 0
+				&& join('', @{$self->{'data'}}
+				[-2 .. -1]) eq '--') {
+
+				err "Bad tag.";
+			}
+			$self->_tag_type;
+			push @{$self->{'data'}}, $self->{'char'};
+
+		# Quote.
+		} elsif ($self->{'spec_stay'} == 4) {
+			if ($self->{'char'} eq $self->{'quote'}) {
+				$self->{'spec_stay'} = $self->{'old_stay'};
+				$self->{'quote'} = '';
+			}
+			push @{$self->{'data'}}, $self->{'char'};
+
+		} elsif ($self->{'char'} eq ']') {
+			push @{$self->{'data'}}, $self->{'char'};
+			$self->{'brace'}--;
+
+		# Next character in normal tag (name).
+		} elsif ($self->{'tag_length'} > 1
+			&& _is_in_tag_name($self->{'char'})) {
+
+			# Comment detect.
+			if (($self->{'tag_length'} == 2
+				|| $self->{'tag_length'} == 3)
+				&& $self->{'char'} eq '-') {
+			
+				$self->{'spec_stay'}++;
+			}
+			if ($self->{'char'} eq '[') {
+				$self->{'brace'}++;
+			}
+			push @{$self->{'data'}}, $self->{'char'};
+			$self->{'tag_length'}++;
+
+		# Other characters.
+		} else {
+			if ($self->{'quote'} eq '' 
+				&& $self->{'char'} eq '"') {
+
+				$self->{'quote'} = '"';
+				$self->{'old_stay'} = $self->{'spec_stay'};
+				$self->{'spec_stay'} = 4;
+			}
+			if ($self->{'quote'} eq '' 
+				&& $self->{'char'} eq "'") {
+
+				$self->{'quote'} = "'";
+				$self->{'old_stay'} = $self->{'spec_stay'};
+				$self->{'spec_stay'} = 4;
+			}
+			if ($self->{'char'} eq '<') {
+				$self->{'bracket'}++;
+			}
+			if ($self->{'char'} eq '[') {
+				$self->{'brace'}++;
+			}
+			$self->_tag_type;
+			push @{$self->{'data'}}, $self->{'char'};
+		}
+	}
+
+	# Remove char from buffer.
+	if ($self->{'stay'} != 99) {
+		if (exists $self->{'text'}) {
+			if (length $self->{'text'} > 1) {
+				$self->{'text'} = substr($self->{'text'}, 1);
+			} else {
+				delete $self->{'text'};
+			}
+		}
+	} else {
+		if (exists $self->{'filename'} 
+			&& defined $self->{'char'}) {
+
+			push @{$self->{'old_data'}}, $self->{'char'}; 
+		}
+	}
+	if ($self->{'stay'} == 98 || $self->{'stay'} == 99) {
+		if ($self->{'stay'} == 99) {
+			$self->{'charpos'}--;
+		}
+	}
+
+	# Next line.
+	if ($self->{'char'} eq "\n") {
+		$self->{'textline'}++;
+		$self->{'charpos'} = 0;
+	}
+}
 
 #------------------------------------------------------------------------------
 sub _is_first_char_of_tag {
