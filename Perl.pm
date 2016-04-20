6eacf440739698420e1987b6ea2b67729596ef5d
Michal Špaček <m.spacek@ibsmm.com>
2016-04-20 16:43:15 +0200

    Prepare for 0.02 version.

--- Perl.pm
+++ Perl.pm
@@ -13,7 +13,7 @@ use Readonly;
 Readonly::Scalar my $EMPTY_STR => q{};
 
 # Version.
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # Constructor.
 sub new {
@@ -646,6 +646,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
